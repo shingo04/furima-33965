f2f2236724f501981e4ad86606de7f0cd139e279
shingo04 <shingobb04@gmail.com>
2021-01-27 17:49:10 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :delivery_fee
+  belongs_to :delivery_source
+  belongs_to :delivery_date
   belongs_to :user
   has_one_attached :image
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :item_status_id
+    validates :delivery_fee_id
+    validates :delivery_source_id
+    validates :delivery_date_id
+  end
 end
