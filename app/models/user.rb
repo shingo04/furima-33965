92e8d90fde7480b68314c5413bd1f7daf94fa5ec
shingo04 <shingobb04@gmail.com>
2021-01-27 12:05:50 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ } do
       validates :last_name
       validates :first_name
     end
