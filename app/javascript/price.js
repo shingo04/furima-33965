4dd04ce345e5b603805ba84caf57b06c8c586fff
shingo04 <shingobb04@gmail.com>
2021-01-27 23:26:28 +0900

    javascriptの修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,8 +1,10 @@
 function price() {
   const textBox = document.getElementById("item-price");
-  const tax = document.getElementById("add-tax-price");
-  const profit = document.getElementById("profit");
-  tax.innerText = (textBox.value) * 0.1;
-  profit.innerText = (textBox.value) - (tax.innerText);
+  textBox.addEventListener('input', () => {
+    const tax = document.getElementById("add-tax-price");
+    const profit = document.getElementById("profit");
+    tax.innerText = (textBox.value) * 0.1;
+    profit.innerText = (textBox.value) - (tax.innerText);
+  });
 };
-window.setInterval(price, 500)
+window.addEventListener('load', price)
