8640721269e7a22bd63905c37475bf5164396d79
shingo04 <shingobb04@gmail.com>
2021-01-28 12:06:22 +0900

    authenticate_user!の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_sign_in, only: [:new]
+  before_action :authenticate_user!, only: [:new]
 
   def index
     
@@ -21,11 +21,5 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:item_name, :explanation, :category_id, :item_status_id, :delivery_fee_id, :delivery_source_id, :delivery_date_id, :item_price, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_sign_in
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
   end
 end
