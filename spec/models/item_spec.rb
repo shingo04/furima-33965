bc43e39e3211dd03abc53af398b3b113a0ca5b67
shingo04 <shingobb04@gmail.com>
2021-01-28 12:47:05 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it "category_idが0では出品できない" do
-        @item.category_id = "0"
+        @item.category_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 0")
       end
@@ -42,7 +42,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Item status can't be blank")
       end
       it "item_status_idが0では出品できない" do
-        @item.item_status_id = "0"
+        @item.item_status_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Item status must be other than 0")
       end
@@ -52,7 +52,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
       end
       it "delivery_fee_idが0では出品できない" do
-        @item.delivery_fee_id = "0"
+        @item.delivery_fee_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery fee must be other than 0")
       end
@@ -62,7 +62,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Delivery source can't be blank")
       end
       it "delivery_source_idが0では出品できない" do
-        @item.delivery_source_id = "0"
+        @item.delivery_source_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery source must be other than 0")
       end
@@ -72,22 +72,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Delivery date can't be blank")
       end
       it "delivery_date_idが0では出品できない" do
-        @item.delivery_date_id = "0"
+        @item.delivery_date_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery date must be other than 0")
       end
       it "item_priceが空では出品できない" do
-        @item.item_price = ""
+        @item.item_price = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Item price can't be blank")
       end
       it "item_priceが300円未満では出品できない" do
-        @item.item_price = "299"
+        @item.item_price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Item price must be greater than or equal to 300")
       end
       it "item_priceが9999999円より多かったら出品できない" do
-        @item.item_price = "10000000"
+        @item.item_price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Item price must be less than or equal to 9999999")
       end
