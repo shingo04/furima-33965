1bff741d231ba952139741d2b3dc75bd01d91dde
shingo04 <shingobb04@gmail.com>
2021-01-29 18:10:59 +0900

    修正（３）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :conditions, only: [:edit, :destroy]
 
   def index
     @items = Item.all.order(id: 'DESC')
@@ -23,9 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless current_user.id == @item.user.id
-      redirect_to root_path
-    end
   end
 
   def update
@@ -43,8 +41,6 @@ class ItemsController < ApplicationController
       else
         render :show
       end
-    else
-      redirect_to root_path
     end
   end
 
@@ -57,5 +53,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def conditions
+    unless current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 end
