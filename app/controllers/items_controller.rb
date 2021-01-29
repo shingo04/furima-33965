13e88bc30518459a58b99ea65f817c11e2d03fc5
shingo04 <shingobb04@gmail.com>
2021-01-29 17:17:22 +0900

    destroyアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,8 +38,12 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if item.destroy
-      redirect_to root_path
+    if current_user.id == item.user.id
+      if item.destroy
+        redirect_to root_path
+      else
+        render :show
+      end
     end
   end
 
