6b2eb4f78dcf3dd1ee58d338a5cf07374d05e94b
shingo04 <shingobb04@gmail.com>
2021-01-29 15:44:40 +0900

    itemsコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless current_user.id == @item.user.id
+    unless user_signed_in? && current_user.id == @item.user.id
       redirect_to root_path
     end
   end
